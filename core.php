9192c64cb3d80a408f89ba3e854bd0d675b87826
JeffMatson <jeff@jeffmatson.net>
2016-08-19 20:25:20 -0400

    Docblocks

--- core.php
+++ core.php
@@ -6,8 +6,23 @@ use Heartbeat_Control\Views\Settings;
 
 class Core {
 
+	/**
+	 * @var object $instance Holds an instance of this class
+	 * @since 2.0.0
+	 * @static
+	 * @access public
+	 */
 	public static $instance;
 
+	/**
+	 * Gets an instance of the Core object
+	 *
+	 * @since 2.0.0
+	 * @static
+	 * @access public
+	 *
+	 * @return Core
+	 */
 	public static function get_instance() {
 		if ( null == self::$instance ) {
 			self::$instance = new self;
@@ -15,10 +30,24 @@ class Core {
 		return self::$instance;
 	}
 
+	/**
+	 * Initializes Heartbeat Control
+	 *
+	 * @since 2.0.0
+	 * @access public
+	 * @see Core::enqueue_actions
+	 */
 	public function init() {
 		$this->enqueue_actions();
 	}
 
+	/**
+	 * Enqueues all actions associated with Heartbeat Control
+	 *
+	 * @since 2.0.0
+	 * @access public
+	 * @todo Add hook
+	 */
 	public function enqueue_actions() {
 		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
@@ -27,14 +56,29 @@ class Core {
 		add_filter( 'heartbeat_settings', array( $this, 'modify_heartbeat' ) );
 	}
 
+	/**
+	 * Enqueues scripts and styles needed by Heartbeat Control
+	 *
+	 * @since 2.0.0
+	 * @access public
+	 * @todo Add hook
+	 *
+	 * @param null|string $hook Reflects the page being accessed. Defaults to null.
+	 */
 	public function enqueue_scripts( $hook = null ) {
 		if ( 'settings_page_heartbeat-control' == $hook ) {
 			wp_enqueue_style( 'heartbeat-control-settings', plugin_dir_url( __FILE__ ) . 'css/settings.css' );
 		}
 	}
 
+	/**
+	 * Initialize the Heartbeat Control menu item
+	 *
+	 * @since 2.0.0 Rewrite
+	 * @since 1.0
+	 * @access public
+	 */
 	public function admin_menu() {
-
 		add_submenu_page(
 			'options-general.php',
 			__( 'Heartbeat Control', 'heartbeat-control' ),
@@ -43,24 +87,41 @@ class Core {
 			'heartbeat-control',
 			array( $this, 'heartbeat_control_menu')
 		);
-
 	}
 
+	/**
+	 * Runs the view when the menu item is accessed
+	 *
+	 * @since 2.0.0
+	 * @access public
+	 * @todo Conditionally load views
+	 */
 	public function heartbeat_control_menu() {
 		$load_view = new Settings();
 		$load_view->init();
 	}
 
+	/**
+	 * Migrates database options to the current version
+	 *
+	 * @since 2.0.0
+	 * @access public
+	 * @todo Write database migration
+	 */
 	public function maybe_migrate_db_options() {
 
 	}
 
-	public function migrate_db() {
-
-	}
-
+	/**
+	 * Determines if the heartbeat should be killed
+	 *
+	 * @since 2.0.0
+	 * @access public
+	 * @todo Clean this up.
+	 *
+	 * @return bool True if the heartbeat was killed.  False, otherwise.
+	 */
 	public function maybe_kill_heartbeat() {
-
 		$kill_heartbeat = false;
 
 		if ( is_admin() ) {
@@ -75,14 +136,6 @@ class Core {
 		}
 
 		$kill_heartbeat = apply_filters( 'hbc_kill_heartbeat', $kill_heartbeat );
-
-//		if ( $this->heartbeat_allowed_locations() ) {
-//			$kill_heartbeat = false;
-//		}
-//
-//		if ( $this->heartbeat_denied_locations() ) {
-//			$kill_heartbeat = true;
-//		}
 
 		if ( is_admin() && $current_screen->id == 'post' ) {
 			$allowed_post_edit_pages = $this->allowed_post_edit_pages();
@@ -107,13 +160,31 @@ class Core {
 		}
 
 		return $kill_heartbeat;
-
 	}
 
+	/**
+	 * Kill the heartbeat.  With fire.
+	 *
+	 * @since 2.0.0
+	 * @access public
+	 * @todo Hook here?
+	 */
 	public function kill_heartbeat() {
 		wp_deregister_script( 'heartbeat' );
 	}
 
+	/**
+	 * Determines if the heartbeat should be modified
+	 *
+	 * @since 2.0.0
+	 * @access public
+	 * @todo Clean this up
+	 * @todo Add hooks
+	 *
+	 * @param array $settings Existing heartbeat settings
+	 *
+	 * @return array Heartbeat settings
+	 */
 	public function maybe_modify_heartbeat( $settings ) {
 
 		$modify_heartbeat = false;
@@ -131,6 +202,18 @@ class Core {
 		return $settings;
 	}
 
+	/**
+	 * Determines if the heartbeat should be fired on the front-end
+	 *
+	 * Uses global defaults.  Could be overridden later.
+	 *
+	 * @since 2.0.0
+	 * @access public
+	 * @todo Hooks
+	 * @todo Cleanup
+	 *
+	 * @return string Returns 'allowed' or 'denied' based on global settings set
+	 */
 	public function frontend_default_enabled() {
 
 		$global_default = get_option( 'hbc_frontend_allowed' );
@@ -143,6 +226,17 @@ class Core {
 
 	}
 
+	/**
+	 * Determines if the heartbeat is fired on the front-end.
+	 *
+	 * Uses global defaults.  Could be overwritten later.
+	 *
+	 * @since 2.0.0
+	 * @access public
+	 * @todo
+	 *
+	 * @return mixed|string|void
+	 */
 	public function admin_default_enabled() {
 
 		$global_default = get_option( 'hbc_admin_allowed' );
