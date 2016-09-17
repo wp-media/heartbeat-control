aeae6c437e19d3d0ad8d4fb75f8cda3ef5781367
JeffMatson <jeff@jeffmatson.net>
2016-09-17 17:37:21 -0400

    Added helper function for converting boolean responses in AJAX calls.

--- ajax.php
+++ ajax.php
@@ -12,6 +12,34 @@ if ( ! current_user_can( 'manage_options' ) && ! check_ajax_referer( 'hbc_settin
 
 class Ajax {
 
+	/**
+	 * Converts a boolean to a string response for AJAX calls.
+	 *
+	 * @since 2.0.0
+	 * @access public
+	 * @static
+	 *
+	 * @param mixed $check The value to check.
+	 * @param bool $echo If the response should be echoed.  Defaults to true.
+	 *
+	 * @return string The response string.
+	 */
+	public static function convert_boolean_response( $check, $echo = true ) {
+
+		if ( $check ) {
+			$response = 'success';
+		} else {
+			$response = 'error';
+		}
+
+		if ( $echo == true ) {
+			echo $response;
+		} else {
+			return $response;
+		}
+
+	}
+
 	public static function update_override() {
 		ob_clean();
 
@@ -22,11 +50,7 @@ class Ajax {
 		if ( ( array_key_exists( $subheader, $overrides['admin'] ) ) && ( array_key_exists( $override_name, $overrides['admin'][$subheader]['items'] ) ) ) {
 			$success = update_option( 'hbc_override_' . $override_name, $_POST['hbc_data']['override_value'] );
 
-			if ( $success ) {
-				echo 'success';
-			} else {
-				echo 'error';
-			}
+			self::convert_boolean_response( $success );
 
 		} else {
 			echo 'error';
@@ -43,11 +67,7 @@ class Ajax {
 		if ( $allowed == ( 'allowed' || 'denied' || 'wp_default' ) ) {
 			$success = update_option( 'hbc_frontend_allowed', $_POST['hbc_data']['frontend_allowed'] );
 
-			if ( $success ) {
-				echo 'success';
-			} else {
-				echo 'error';
-			}
+			self::convert_boolean_response( $success );
 
 		} else {
 			echo 'error';
@@ -65,11 +85,7 @@ class Ajax {
 		if ( $allowed == ( 'allowed' || 'denied' || 'wp_default' ) ) {
 			$success = update_option( 'hbc_admin_allowed', $_POST['hbc_data']['admin_allowed'] );
 
-			if ( $success ) {
-				echo 'success';
-			} else {
-				echo 'error';
-			}
+			self::convert_boolean_response( $success );
 
 		} else {
 			echo 'error';
@@ -87,11 +103,7 @@ class Ajax {
 		if ( is_numeric( $interval ) ) {
 			$success = update_option( 'hbc_frontend_interval', $_POST['hbc_data']['frontend_interval'] );
 
-			if ( $success ) {
-				echo 'success';
-			} else {
-				echo 'error';
-			}
+			self::convert_boolean_response( $success );
 
 		} else {
 			echo 'error';
@@ -109,11 +121,7 @@ class Ajax {
 		if ( is_numeric( $interval ) ) {
 			$success = update_option( 'hbc_admin_interval', intval( $_POST['hbc_data']['admin_interval'] ) );
 
-			if ( $success ) {
-				echo 'success';
-			} else {
-				echo 'error';
-			}
+			self::convert_boolean_response( $success );
 
 		} else {
 			echo 'error';
