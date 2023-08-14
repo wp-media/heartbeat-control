3f088b4f05c67ec86915d36b57d6e7c176785a99
Rémy Perona <remperona@gmail.com>
2023-08-14 12:26:28 -0400

    update cmb2

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ac6696c970fbcac212058001d6edc0df015587d2',
+        'name' => 'wp-media/heartbeat-control',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cmb2/cmb2' => array(
+            'pretty_version' => 'v2.10.1',
+            'version' => '2.10.1.0',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../cmb2/cmb2',
+            'aliases' => array(),
+            'reference' => '4afc4bb7b92ab6d93aac2247c9a84af773e42532',
+            'dev_requirement' => false,
+        ),
+        'wp-media/heartbeat-control' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ac6696c970fbcac212058001d6edc0df015587d2',
+            'dev_requirement' => false,
+        ),
+    ),
+);
